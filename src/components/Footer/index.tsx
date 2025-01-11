360a4aae06db635403f87ba5ef338c8addd4077c
Aishat Agbolade <aishaagbolade@gmail.com>
2025-01-11 19:55:23 +0000

    refactor(footer): made necessary changes in footer component based on PR feedback

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,26 +3,23 @@ import styles from "./style.module.css";
 import { footerLinks } from "../../constants";
 
 const Footer = () => {
+  const thisYear = new Date().getFullYear();
   return (
     <footer className={styles.footer}>
       <div className={styles.footerColumns}>
         {footerLinks?.map(({ id, links }) => (
-          <ul
-            className={styles.footerLinks}
-            key={id}
-            aria-labelledby={`footer-section-${id}`}
-          >
+          <ul className={styles.footerLinks} key={id}>
             {links?.map(({ id, title: linkText, url }) => (
               <li key={id}>
                 <a href={url} aria-label={linkText}>
-                  {linkText}{" "}
+                  {linkText}
                 </a>
               </li>
             ))}
           </ul>
         ))}
       </div>
-      <p>&copy; {new Date().getFullYear()} Invest. All rights reserved.</p>
+      <p>&copy; {thisYear} Invest. All rights reserved.</p>
     </footer>
   );
 };
